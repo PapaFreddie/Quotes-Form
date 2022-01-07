5343f858d700bcb5cb5c3bb13878b84b1380472a
PapaFreddie <fred.papa@student.moringaschool.com>
2022-01-07 12:05:40 +0300

    created mock test file for quotes files

---
+++ src/app/mock-quotes.ts
@@ -0,0 +1,26 @@
+export const QUOTES = [
+    {
+        id: 1,
+        text: '"There is no path to happiness; happiness is the path."',
+        author: 'Buddha',
+        inspirational: true,
+    },
+    {
+        id: 2,
+        text: '"There is no path to happiness; happiness is the path."',
+        author: 'Buddha',
+        inspirational: true,
+    },
+    {
+        id: 3,
+        text: '"There is no path to happiness; happiness is the path."',
+        author: 'Buddha',
+        inspirational: true,
+    },
+    {
+        id: 4,
+        text: '"There is no path to happiness; happiness is the path."',
+        author: 'Buddha',
+        inspirational: true,
+    },
+]
