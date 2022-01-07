a49d77b55e6a45ef8dc3b7a5002a670e81387b40
PapaFreddie <fred.papa@student.moringaschool.com>
2022-01-07 19:38:05 +0300

    added styles to quote-type component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UiService } from 'src/app/services/ui.service';
 import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -13,7 +14,7 @@ export class HeaderComponent implements OnInit {
   subscription!: Subscription;
 
 
-  constructor(private uiService: UiService) {
+  constructor(private uiService: UiService, private router: Router) {
     this.subscription = this.uiService.onToggle().subscribe(value => this.showAddQuote = value)
    }
 
@@ -21,6 +22,10 @@ export class HeaderComponent implements OnInit {
   }
   toggleAddQuote(){
     this.uiService.toggleAddQuote();
+    }
+    hasRoute(route: string){
+      return this.router.url === route;
+      
     }
 
 }
