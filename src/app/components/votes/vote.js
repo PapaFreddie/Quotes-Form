a7dfd8ac075506e503901532a87feb116510c086
PapaFreddie <fred.papa@student.moringaschool.com>
2022-01-10 08:09:48 +0300

    update everything

--- src/app/components/votes/vote.js
+++ src/app/components/votes/vote.js
@@ -10,3 +10,15 @@ let likebtn = document.querrySelector('#likebtn');
         dislikebtn.addEventLister('click', () =>{
             input1.value = parseInt(input1.value) + 1;
         })
+        let likebtn = document.querrySelector('#likebtn');
+        let dislikebtn = document.querrySelector('#dislikebtn');
+        let input0 = document.querrySelector('#input0');
+        let input1 = document.querrySelector('#input1');
+    
+    
+        likebtn.addEventLister('click', () =>{
+            input0.value = parseInt(input0.value) + 1;
+        })
+        dislikebtn.addEventLister('click', () =>{
+            input1.value = parseInt(input1.value) + 1;
+        })
